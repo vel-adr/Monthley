402d31bf6544f086c3f9aebe4618c3bc965f07d4
vel-adr <anselmus.pavel@gmail.com>
2022-04-27 15:21:06 +0700

    Initial Commit

---
+++ NanoChallenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NanoChallenge1
+//
+//  Created by Anselmus Pavel Adriska on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
